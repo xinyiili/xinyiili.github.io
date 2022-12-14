42774a402f2cde107025df3ff0644fa06b08ab5e
xinyiili <119898093+xinyiili@users.noreply.github.com>
2022-12-14 17:59:23 -0500

    Update msc2012.js

--- msc2012.js
+++ msc2012.js
@@ -3,7 +3,7 @@
 function rangeValue(value) {
   const root = document.documentElement;
 
-  //Change opacity of image 1 as value of range slider increases (i.e., as slider button is dragged from left to right)
+  //Decrease opacity of image 1 as value of range slider increases (i.e., as slider button is dragged from left to right)
   root.style.setProperty('--opacityImg1', (100 - value) / 100);
   root.style.setProperty('--opacityImg2', 100);
   
